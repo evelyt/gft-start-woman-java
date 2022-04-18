a780c623b584221fd7ef90bc0f8efdc294028097
Evely Tereza <terezaevely@gmail.com>
2022-04-17 23:02:42 -0300

    Lógica Condicional e Controle de Fluxos

---
+++ lgc-condicional-e-controle-fluxo/src/com/dio/Main.java
@@ -0,0 +1,13 @@
+package com.dio;
+
+import com.dio.opRelacionais.OperadoresRelacionais;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		OperadoresRelacionais.exibe();
+
+	}
+
+}
---
+++ lgc-condicional-e-controle-fluxo/src/com/dio/opLogicos/OperadoresLogicos.java
@@ -0,0 +1,13 @@
+package com.dio.opLogicos;
+
+public class OperadoresLogicos {
+	
+	/*Conjuncao:
+	 * Disjuncao:
+	 * Disjuncao Exclusiva:
+	 * Negacao:
+	*/
+	
+	
+
+}
